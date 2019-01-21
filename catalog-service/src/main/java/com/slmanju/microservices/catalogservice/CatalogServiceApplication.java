7976d7044a6dd257802cdb31a894cc3361c455c1
Manjula Jayawardana <manjulajayawardana@gmail.com>
2019-01-21 21:40:01 +0530

    Initial commit

---
+++ catalog-service/src/main/java/com/slmanju/microservices/catalogservice/CatalogServiceApplication.java
@@ -0,0 +1,14 @@
+package com.slmanju.microservices.catalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogServiceApplication.class, args);
+    }
+
+}
+
---
+++ catalog-service/src/main/java/com/slmanju/microservices/catalogservice/domain/Product.java
@@ -0,0 +1,23 @@
+package com.slmanju.microservices.catalogservice.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ms_product")
+@Data @NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(generator="uuid2")
+    @GenericGenerator(name="uuid2", strategy = "uuid")
+    private String id;
+    private Integer price;
+
+}
