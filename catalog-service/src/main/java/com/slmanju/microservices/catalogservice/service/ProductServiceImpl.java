2ef397ba31d3ea04d8cf27bd7d192e90731b2e89
Manjula Jayawardana <manjulajayawardana@gmail.com>
2019-01-21 22:42:53 +0530

    Complete basic structure

--- catalog-service/src/main/java/com/slmanju/microservices/catalogservice/service/ProductServiceImpl.java
+++ catalog-service/src/main/java/com/slmanju/microservices/catalogservice/service/ProductServiceImpl.java
@@ -2,6 +2,7 @@ package com.slmanju.microservices.catalogservice.service;
 
 import com.slmanju.microservices.catalogservice.domain.Product;
 import com.slmanju.microservices.catalogservice.domain.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,7 @@ public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
 
+    @Autowired
     public ProductServiceImpl(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
