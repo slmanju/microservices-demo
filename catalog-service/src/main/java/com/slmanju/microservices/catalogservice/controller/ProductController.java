b2d7cf0f322f27e4f240334ad6bf67b7e31a6218
Manjula Jayawardana <manjulajayawardana@gmail.com>
2019-01-27 12:12:26 +0530

    integrate ribbon

--- catalog-service/src/main/java/com/slmanju/microservices/catalogservice/controller/ProductController.java
+++ catalog-service/src/main/java/com/slmanju/microservices/catalogservice/controller/ProductController.java
@@ -2,7 +2,10 @@ package com.slmanju.microservices.catalogservice.controller;
 
 import com.slmanju.microservices.catalogservice.service.ProductService;
 import com.slmanju.microservices.catalogservice.service.ProductView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +16,12 @@ import java.util.List;
 @RestController
 public class ProductController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
+
     private final ProductService productService;
+
+    @Autowired
+    private Environment environment;
 
     @Autowired
     public ProductController(ProductService productService) {
@@ -27,6 +35,7 @@ public class ProductController {
 
     @GetMapping("/{id}")
     public ResponseEntity<ProductView> findById(@PathVariable("id") String id) {
+        LOGGER.info(":: running on :: " + environment.getProperty("local.server.port"));
         return ResponseEntity.ok(productService.findById(id));
     }
 
--- shopping-cart/src/main/java/com/slmanju/microservices/shoppingcart/service/CatalogService.java
+++ shopping-cart/src/main/java/com/slmanju/microservices/shoppingcart/service/CatalogService.java
@@ -1,11 +1,13 @@
 package com.slmanju.microservices.shoppingcart.service;
 
 import com.slmanju.microservices.shoppingcart.service.view.Product;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "catalog-service", url = "localhost:8010")
+@RibbonClient(name = "catalog-service")
+@FeignClient(name = "catalog-service")
 public interface CatalogService {
 
     @GetMapping("/{id}")
